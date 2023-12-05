45cc8fdb70b5f853bf2fc926446f35a961b01410
Sammy Shehter <sammyshehter@gmail.com>
2023-12-05 07:15:49 +0200

    day 5 start

---
+++ adventofcode/2023/day5/part1.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const x = fs.readFileSync('input1.txt').toString()
+let match = []
+
+console.log(match)
---
+++ adventofcode/2023/day5/part2.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const x = fs.readFileSync('input.txt').toString().split('\n')
+const match = []
+
+console.log(match)
